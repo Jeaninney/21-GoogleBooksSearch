0e470a6bc9ddb4c0c0cbbf88f929a47aaeb3ab9c
Jeaninne <jeaninnel@hotmail.com>
2020-07-07 02:25:05 -0400

    removed unused components

--- client/src/components/BooksList/index.js
+++ client/src/components/BooksList/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import API from "../../utils/API";
 import "./style.css";
 
-const imageurlA = "https://books.google.com/books/content?id=";
-const imageurlB = "&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api";
-const linkurl = "https://www.google.com/books/edition/_/";
-
 // Separate the UI specific transforming logic to utils folder
 //import { bookAuthors } from '../utils';
 const bookAuthors = (authors) => {
@@ -55,7 +51,7 @@ const Book = ({ book, page }) => {
 		<div className= "mg-5 border-bottom border-warning rounded-lg">
 			<div className="row" key={book.id}>
 				<div className="col title ">
-					
+					{book.volumeInfo.title}
 					<button type="button" onClick={() => handleSave(book)} className="btn btn-warning save">
 					{page === "search" ? "Save" : "Delete"}
 					</button>
@@ -63,6 +59,7 @@ const Book = ({ book, page }) => {
 						<a
 							href={book.volumeInfo.infoLink}
 							target="_blank"
+							rel="noopener noreferrer"
 						>
 							View
 						</a>
